4182e0abdc0313a18362ae9e0afe9b9e4b5b56b3
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-02-28 23:20:40 -0300

    [NG] WORKING_WITH_SIGNALS - feat: implemented the clickCount number signal in the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal, effect } from '@angular/core';
 import { interval, map } from 'rxjs';
 import { DestroyRef } from '@angular/core';
 
@@ -9,19 +9,30 @@ import { DestroyRef } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0)
   private destroyRef = inject(DestroyRef)
 
-  ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(
-        map((val) => val * 2)
-      )
-      .subscribe({
-        next: (val) => console.log(val)
-      })
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe()
+  constructor() {
+    effect(() => {
+      console.log(this.clickCount())
     })
+  }
+
+  ngOnInit(): void {
+    // const subscription = interval(1000)
+    //   .pipe(
+    //     map((val) => val * 2)
+    //   )
+    //   .subscribe({
+    //     next: (val) => console.log(val)
+    //   })
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe()
+    // })
+  }
+
+  onClick() {
+    this.clickCount.update((prev) => prev + 1)
   }
 }
